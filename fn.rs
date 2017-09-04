b707e7c3010f0002a980f7c3159b9ece3f5d29a6
Haruka Kai <plalal@gmail.com>
2017-09-04 23:29:29 +0900

    fn

--- fn.rs
+++ fn.rs
@@ -2,13 +2,19 @@ fn add(x:isize, y:isize) -> isize {
 	(x + y) % 42
 }
 
+fn mul(x:isize, y:isize) -> isize {
+	(x * y) % 42
+}
+
 fn main() {
-	let f: fn(isize, isize) -> isize = add;
-	for i in 0 .. 30 {
-		for j in 0 .. 30 {
-			let s = f( i, j );
-			let p:&str = if s < 10 { " " } else { "" };
-			print!("| {}{}", p, s);
+	let fs: [ fn(isize, isize) -> isize; 2 ] = [ add, mul ];
+	for i in 0 .. 20 {
+		for j in 10 .. 30 {
+			for f in &fs {
+				let s = f( i, j );
+				let p:&str = if s < 10 { " " } else { "" };
+				print!("| {}{}", p, s);
+			}
 		}
 		println!(" |");
 	}
