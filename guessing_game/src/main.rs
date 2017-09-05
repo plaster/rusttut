e2800a2db643918370d4842e0b32a481874862b7
Haruka Kai <plalal@gmail.com>
2017-09-06 00:52:35 +0900

    rand

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
+extern crate rand;
+
 use std::io::{self, Write};
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+	let secret_number = rand::thread_rng().gen_range(1, 101);
+	println!("Generated secret number: {}", secret_number);
     print!("input your guess: ");
 	io::stdout().flush().ok().expect("failed to flush");
 	let mut guess = String::new();
