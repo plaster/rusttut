4f73133f33beeb28ac18b5aec4ae6275b0239cab
Haruka Kai <plalal@gmail.com>
2017-09-06 00:00:56 +0900

    input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    print!("input your guess: ");
+	io::stdout().flush().ok().expect("failed to flush");
+	let mut guess = String::new();
+	io::stdin().read_line(&mut guess)
+		.expect("failed to read line");
+	println!("you guessed {}", guess);
 }
