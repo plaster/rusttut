50ba815599144f26c75ee19b4c034274a1093f71
Haruka Kai <plalal@gmail.com>
2017-09-07 22:51:22 +0900

    loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,18 +8,24 @@ fn main() {
     println!("Guess the number!");
 	let secret_number = rand::thread_rng().gen_range(1, 101);
 	println!("Generated secret number: {}", secret_number);
-    print!("input your guess: ");
-	io::stdout().flush().ok().expect("failed to flush");
-	let mut guess = String::new();
-	io::stdin().read_line(&mut guess)
-		.expect("failed to read line");
-	println!("you guessed {}", guess);
-	let guess: u32 = guess.trim().parse()
-		.expect("failed to parse as number");
 
-	match guess.cmp(&secret_number) {
-		Ordering::Less => println!("Too small"),
-		Ordering::Greater => println!("Too big"),
-		Ordering::Equal => println!("Just!"),
+	loop {
+		print!("input your guess: ");
+		io::stdout().flush().ok().expect("failed to flush");
+		let mut guess = String::new();
+		io::stdin().read_line(&mut guess)
+			.expect("failed to read line");
+		println!("you guessed {}", guess);
+		let guess: u32 = guess.trim().parse()
+			.expect("failed to parse as number");
+
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too small"),
+			Ordering::Greater => println!("Too big"),
+			Ordering::Equal => {
+				println!("Just!");
+				break;
+			},
+		}
 	}
 }
