e908f4d3a0038b88f318c56b0ecf02f7f1d66cdd
Haruka Kai <plalal@gmail.com>
2017-09-13 01:25:26 +0900

    square

--- rectangle.rs
+++ rectangle.rs
@@ -17,6 +17,9 @@ impl Rectangle {
 	fn can_hold(&self, other: &Rectangle) -> bool {
 		self.width > other.width && self.height > other.height
 	}
+	fn square(size: u32) -> Rectangle {
+		Rectangle{ width: size, height: size }
+	}
 }
 
 fn main() {
@@ -26,8 +29,24 @@ fn main() {
 	println!("r2 = {:?}; area = {}", r2, r2.area());
 	let r3 = Rectangle{ width: 15, height: 20 };
 	println!("r3 = {:?}; area = {}", r3, r3.area());
+	let r4 = Rectangle::square(80);
+	println!("r4 = {:?}; area = {}", r4, r4.area());
+
+	println!("r1 can hold r2: {}", r1.can_hold(&r2));
+
+	println!("r2 can hold r1: {}", r2.can_hold(&r1));
+
 	println!("r1 can hold r3: {}", r1.can_hold(&r3));
-	println!("r3 can hold r1: {}", r3.can_hold(&r1));
 	println!("r2 can hold r3: {}", r2.can_hold(&r3));
+
+	println!("r3 can hold r1: {}", r3.can_hold(&r1));
 	println!("r3 can hold r2: {}", r3.can_hold(&r2));
+
+	println!("r1 can hold r4: {}", r1.can_hold(&r4));
+	println!("r2 can hold r4: {}", r2.can_hold(&r4));
+	println!("r3 can hold r4: {}", r3.can_hold(&r4));
+
+	println!("r4 can hold r1: {}", r4.can_hold(&r1));
+	println!("r4 can hold r2: {}", r4.can_hold(&r2));
+	println!("r4 can hold r3: {}", r4.can_hold(&r3));
 }
