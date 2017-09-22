7289329ad8a14e05d4f41393470a2fca7289981e
Haruka Kai <plalal@gmail.com>
2017-09-22 09:52:35 +0900

    wasm-hello

--- wasm-hello.rs
+++ wasm-hello.rs
@@ -5,5 +5,9 @@ fn main() {
 ~/opt/emsdk-portable% ./emsdk install latest
 % source ~/opt/emsdk-portable/emsdk_env.sh
 % emsdk install sdk-incoming-64bit
+/usr/bin/ld: error: ../../../../bin/clang-check(.debug_info) is too large (0x2a7a526f bytes)
+% df -h ~
+Filesystem                   Size  Used Avail Use% Mounted on
+/dev/mapper/cymbal--vg-root   36G   32G  1.6G  96% /
 % emsdk activate sdk-incoming-64bit
 */
